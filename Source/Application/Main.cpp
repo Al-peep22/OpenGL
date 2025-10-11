0a2e304be6338183682c7dcbc505bcf4f653802f
Al-peep22 <awaldsmith@student.neumont.edu>
2025-10-10 23:37:28 -0600

    My atempt at fixing everything and shader  assignment

--- Source/Application/Main.cpp
+++ Source/Application/Main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
     // initialize scene
 
 
-    // create vectors
+	// OPEN_GL SETUP
     std::vector<neu::vec3> points = {
         {0, 0.5, 0},
         {-0.5, -0.5, -0.5},
@@ -21,6 +21,94 @@ int main(int argc, char* argv[]) {
         {1, 1, 0},  // yellow
 		{1, 0, 1}   // pink
     };
+
+    GLuint vbo[2];
+    glGenBuffers(2, vbo);
+
+    // Vertex Buffer (position)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(neu::vec3) * points.size(), points.data(), GL_STATIC_DRAW);
+
+    // Vertex Buffer (color)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(neu::vec3) * colors.size(), colors.data(), GL_STATIC_DRAW);
+
+    // Vertex Array
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Position
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    // Color
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    // Vertex Shader
+    std::string vs_source;
+    neu::file::ReadTextFile("Shaders/basic.vert", vs_source);
+    const char* vs_cstr = vs_source.c_str();
+
+    GLuint vs;
+    vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vs_cstr, NULL);
+    glCompileShader(vs);
+    int success;
+    glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        std::string infoLog(512, '\0');  // pre-allocate space
+        GLsizei length;
+        glGetShaderInfoLog(vs, (GLsizei)infoLog.size(), &length, &infoLog[0]);
+        infoLog.resize(length);
+
+        LOG_WARNING("Shader compilation failed: {}", infoLog);
+    }
+
+    // Fragment shader
+    std::string fs_source;
+    neu::file::ReadTextFile("Shaders/basic.frag", fs_source);
+    const char* fs_cstr = fs_source.c_str();
+
+    GLuint fs;
+    fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fs_cstr, NULL);
+    glCompileShader(fs);
+    glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        std::string infoLog(512, '\0');  // pre-allocate space
+        GLsizei length;
+        glGetShaderInfoLog(fs, (GLsizei)infoLog.size(), &length, &infoLog[0]);
+        infoLog.resize(length);
+
+        LOG_WARNING("Shader compilation failed: {}", infoLog);
+    }
+
+    // Program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        std::string infoLog(512, '\0');  // pre-allocate space
+        GLsizei length;
+        glGetProgramInfoLog(shaderProgram, (GLsizei)infoLog.size(), &length, &infoLog[0]);
+        infoLog.resize(length);
+
+        LOG_WARNING("Program link failed: {}", infoLog);
+    }
+    glUseProgram(shaderProgram);
+
+    // Uniform
+    GLint uniform = glGetUniformLocation(shaderProgram, "u_time");
+    ASSERT_MSG(uniform != -1, "Could not find uniform u_time.");
 
     SDL_Event e;
     bool quit = false;
@@ -39,31 +127,39 @@ int main(int argc, char* argv[]) {
         // ESC CHECK
         if (neu::GetEngine().GetInput().GetKeyPressed(SDL_SCANCODE_ESCAPE)) quit = true;
 
-        // CLEAR SCREEN
-        neu::vec3 bgColor{ 0, 0, 0 };
-        neu::GetEngine().GetRenderer().SetColor(bgColor.r, bgColor.g, bgColor.b);
+		// UPDATE UNIFORMS
+		glUniform1f(uniform, neu::GetEngine().GetTime().GetTime());
+
+  //      // CLEAR SCREEN
+  //      neu::vec3 bgColor{ 0, 0, 0 };
+  //      neu::GetEngine().GetRenderer().SetColor(bgColor.r, bgColor.g, bgColor.b);
+  //      neu::GetEngine().GetRenderer().Clear();
+
+		//// UPDATE SCENE
+        /*
+        float angle = neu::GetEngine().GetTime().GetTime() * 90.0f;
+        float scale = neu::math::Remap(-1.0f, 1.0f, 0.3f, 1.5f, neu::math::sin(neu::GetEngine().GetTime().GetTime()));
+        neu::vec2 mouse = neu::GetEngine().GetInput().GetMousePosition();
+        neu::vec2 position;
+        position.x = neu::math::Remap(0.0f, (float)neu::GetEngine().GetRenderer().GetWidth(), -1.0f, 1.0f, mouse.x);
+        position.y = -neu::math::Remap(0.0f, (float)neu::GetEngine().GetRenderer().GetHeight(), -1.0f, 1.0f, mouse.y);
+        */
+
+
+        // draw
+        neu::vec3 color{ 0, 0, 0 };
+        neu::GetEngine().GetRenderer().SetColor(color.r, color.g, color.b);
         neu::GetEngine().GetRenderer().Clear();
 
-        // GET TIME
-        float time = neu::GetEngine().GetTime().GetTime();
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, (GLsizei)points.size());
 
-        // GET MOUSE POSITION
-        neu::vec2 mousePos = neu::GetEngine().GetInput().GetMousePosition();
+        /*glLoadIdentity();
+        glPushMatrix();
 
-        // Normalize mouse position
-        float tx = (mousePos.x / 800.0f) * 2.0f - 1.0f;
-        float ty = -((mousePos.y / 600.0f) * 2.0f - 1.0f); // flip Y
-
-        // Calculate rotation and scale
-        float angle = time * 50.0f; // degrees per second
-        float scale = std::sin(time) * 0.5f + 1.0f; // 0.5 to 1.5
-
-        // Apply transformations
-        glPushMatrix(); // Save current transform
-
-        glTranslatef(tx, ty, 0.0f);         // mouse translation
-        glRotatef(angle, 0.0f, 1.0f, 0.0f); // rotation around y-axis
-        glScalef(scale, scale, scale);     // scaling
+        glTranslatef(position.x, position.y, 0);
+        glRotatef(angle, 0, 0, 1);
+        glScalef(scale, scale, 1);*/
 
         // PRIMATIVE TYPES
         /*
@@ -79,7 +175,7 @@ int main(int argc, char* argv[]) {
         * GL_POLYGON
         * etc.
         */
-        glBegin(GL_TRIANGLES); 
+        /*glBegin(GL_TRIANGLES);
 
         for (size_t i = 0; i < points.size(); ++i) {
             glColor3f(colors[i].r, colors[i].g, colors[i].b);
@@ -87,10 +183,33 @@ int main(int argc, char* argv[]) {
         }
 
         glEnd();
-        glPopMatrix(); // Restore transform state
 
-        // Present frame
+        glPopMatrix();*/
+
         neu::GetEngine().GetRenderer().Present();
+
+        //// GET TIME
+        //float time = neu::GetEngine().GetTime().GetTime();
+
+        //// GET MOUSE POSITION
+        //neu::vec2 mousePos = neu::GetEngine().GetInput().GetMousePosition();
+
+        //// Normalize mouse position
+        //float tx = (mousePos.x / 800.0f) * 2.0f - 1.0f;
+        //float ty = -((mousePos.y / 600.0f) * 2.0f - 1.0f); // flip Y
+
+        //// Calculate rotation and scale
+        //float angle = time * 50.0f; // degrees per second
+        //float scale = std::sin(time) * 0.5f + 1.0f; // 0.5 to 1.5
+
+        //// Apply transformations
+        //glPushMatrix(); // Save current transform
+
+        //glTranslatef(tx, ty, 0.0f);         // mouse translation
+        //glRotatef(angle, 0.0f, 1.0f, 0.0f); // rotation around y-axis
+        //glScalef(scale, scale, scale);     // scaling
+
+        
     }
 
     neu::GetEngine().Shutdown();
